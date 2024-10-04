3046ce889d100a638366cc70f72c06de1d49c427
VarNotUsed <varnotused@protonmail.com>
2024-10-04 15:47:48 +0200

    lets simply redirect default page to first service page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,8 @@
 "use client";
-import TaskQueue from "@/components/TaskQueue/TaskQueue";
-import { useEffect, useState } from "react";
-import { TaskQueueEntryProps } from "@/types";
-import { fetchTasks } from "@/utils/tasks";
-import SubmitTask from "@/components/AddTask/AddTask";
+import { redirect } from 'next/navigation'
+import { taskQueueAddresses } from "@/utils";
 
 /* eslint-disable @next/next/no-img-element */
 export default function Home() {
-  const [taskQueue, setTaskQueue] = useState<TaskQueueEntryProps[]>([]);
-
-  const getTasks = async () => {
-    const allTasks = await fetchTasks();
-    setTaskQueue(allTasks);
-  };
-
-  useEffect(() => {
-    getTasks();
-
-    const intervalId = setInterval(() => {
-      getTasks();
-    }, 3000); 
-
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, []);
-
-  return (
-    <div>
-      <section>
-        <h1 className="font-bold text-text-primary text-[26px] mb-4">Services</h1>
-        <div className="grid grid-cols-4 gap-4 pb-[40px] mb-[40px] border-b border-border-primary">
-          <SubmitTask />
-        </div>
-      </section>
-      <section>
-        <h1 className="font-bold text-text-primary text-[26px] mb-4">Task Queue</h1>
-        <TaskQueue entries={taskQueue} />
-      </section>
-    </div>
-  );
+  redirect(`/avs/oracle/${taskQueueAddresses[0].address}`);
 }
